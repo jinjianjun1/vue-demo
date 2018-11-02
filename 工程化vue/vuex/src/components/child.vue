1255a79fd6ccea7dbb861fa984a516ae417fe0f6
jason <13141374484@163.com>
2018-11-02 21:40:47 +0800

    vuex

---
+++ 工程化vue/vuex/src/components/child.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+    <span @click="deliver"> 我是子组件</span> <br>
+    {{msg}} 
+    <hr>
+    {{getCount}}
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'child',
+        data:function(){
+            return {
+                childmsg:'我是子组件内容'
+            }
+        },
+        props:{
+            msg:{
+                type:String,
+                default:''
+            }
+        },
+        methods:{
+            deliver:function(){
+               // let childmsg=this.childmsg+'我从子组件来',
+                this.$emit('comehere', this.childmsg)
+            }
+        },
+        computed:{
+            getCount:function(){
+                return this.$store.state.count
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    
+</style>
---
+++ 工程化vue/vuex/src/components/parent.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+    <span> 我是父组件</span> -----{{fmsg}}
+    <hr>
+    <child :msg='msg' @comehere='getmsg'>
+    
+    </child>
+    </div>
+</template>
+
+<script>
+    import child from './child'
+    export default {
+        name:'parent',
+        data:function(){
+            return {
+                msg:'我是父组件的内容',
+                fmsg:''
+            }
+        },
+        components:{
+            child
+        },
+        methods:{
+            getmsg:function(value){
+                this.fmsg=value
+            }
+        }
+    }    
+</script>
+
+<style scoped>
+    
+</style>
